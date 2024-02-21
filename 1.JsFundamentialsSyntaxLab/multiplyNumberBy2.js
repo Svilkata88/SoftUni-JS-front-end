b28c30b6c275174b76525df982dc3b6ce4ddd1a7
Svilen Ivanov <134698218+Svilkata88@users.noreply.github.com>
2024-02-21 23:51:58 +0200

    first 3 tasks for this repo

---
+++ 1.JsFundamentialsSyntaxLab/multiplyNumberBy2.js
@@ -0,0 +1,8 @@
+function multiply(num) {
+    return num * 2
+}
+
+
+console.log(multiply(2))
+
+console.log(3)
---
+++ 1.JsFundamentialsSyntaxLab/studentInformation.js
@@ -0,0 +1,5 @@
+function studentInfo(name, age, grade) {
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`)
+}
+
+studentInfo('Svilen', '35', 6.00);
